872dce2d84813543ce25c489338d92c637ff2ce1
aprilharris <54374224+aprilharris@users.noreply.github.com>
2024-04-17 19:00:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Challenge 1
+// MANUALLY LOADING ELEMENTS FROM AN ARRAY INTO BROWSER
+
 let puppies = [
   //0
   "img/puppy-1.jpg",
@@ -8,34 +9,36 @@ let puppies = [
   "img/puppy-3.jpg",
 ];
 
-$(".puppies").append(`<img src=${puppies[0]} >`);
-$(".puppies").append(`<img src=${puppies[1]} >`);
-$(".puppies").append(`<img src=${puppies[2]} >`);
+$("div.puppies").append(`<img src=${puppies[0]} />`);
+$("div.puppies").append(`<img src=${puppies[1]} />`);
+$("div.puppies").append(`<img src=${puppies[2]} />`);
 
-// Challenge 2
+
+
 let avengerPics = [
   "img/avenger1.jpg",
   "img/avenger2.jpg",
-  "img/avenger3.jpg",
-  "img/avenger4.jpg",
-  "img/avenger5.jpg",
-  "img/avenger6.jpg"
+  "img/avenger3.jpg"
 ];
+
 
 let avengerNames = [
-  "Iron Man",
-  "Captain America",
-  "The Incredible Hulk",
-  "Black Widow",
-  "Groot",
-  "Gamora"
+'IRON MAN',
+'CAPTAIN AMERICA',
+'THE HULK'
 ];
 
+
 function fetchAvengers() {
-  for(let i=0; i<avengerPics.length; i++) {
-    $('#avengers').append(`<img src='${avengerPics[i]}' />`)
-    $('#avengers').append(`<h2>${avengerNames[i]}</>`)
-  }
-   
+
+for(let i=0; i<avengerPics.length; i++) {
+  
+$("div.avengers").append(`<img src="${avengerPics[i]}" />`);
+  $("div.avengers").append(`<h3>${avengerNames[i]}</h3>`);
+
+}
+  
 };
+
+
 
